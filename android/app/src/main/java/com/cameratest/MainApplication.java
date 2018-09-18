a26226f5483fc6dd8ab83d8b96f1ede1b1c68116
Leandro Nishijima <leandro.nishijima@gmail.com>
2018-09-18 15:46:46 -0300

    initial commit + react-native-camera config

--- android/app/src/main/java/com/cameratest/MainApplication.java
+++ android/app/src/main/java/com/cameratest/MainApplication.java
@@ -3,6 +3,7 @@ package com.cameratest;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             new ReactNativeI18n(),
             new VectorIconsPackage(),
             new RNDeviceInfo()
